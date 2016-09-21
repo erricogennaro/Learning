77348c246ce1657302263f3d57b9953e1a88adfb
dev <dev@wsx56.mfti>
2016-09-21 12:47:26 +0200

    refactoring

---
+++ RandomAnnouncer/src/ch/gmtech/gennaro/learning/randomannouncer/RandomValue.java
@@ -0,0 +1,17 @@
+package ch.gmtech.gennaro.learning.randomannouncer;
+import java.util.Random;
+
+
+public class RandomValue {
+	private int value;
+ 
+	public RandomValue(int persons){
+	Random rand = new Random(); 
+	value = rand.nextInt(persons);
+	}
+	
+	public int getValue(){
+		return value;
+	}
+	
+}
